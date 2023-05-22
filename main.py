03bbf2fb282b9e3fc7e3e9aa3779d7697e2295d1
shruthim0 <shmalayath@gmail.com>
2023-05-22 12:59:48 -0700

    addingreviewtomain.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
 from model.college import initCollege
+from model.reviews import initReviews
 
 
 # setup APIs
@@ -17,6 +18,7 @@ from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
 from api.college import player_college
+from api.review import review_api
 
 
 # setup App pages
@@ -33,6 +35,7 @@ app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(college_api)
 app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(review_api)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -52,6 +55,7 @@ def activate_job():  # activate these items
     initJokes()
     initUsers()
     initPlayers()
+    initReviews()
 
 # this runs the application on the development server
 if __name__ == "__main__":
